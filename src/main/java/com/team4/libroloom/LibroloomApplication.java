2f8788ecafda0c7a4894ef7bfbbad9e792fd1c49
stevencandrink <nhatngau10@gmail.com>
2024-01-20 16:50:18 +0700

    deploy v3

--- src/main/java/com/team4/libroloom/LibroloomApplication.java
+++ src/main/java/com/team4/libroloom/LibroloomApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibroloomApplication {
 
-	public String PGUSER=System.getenv("PGUSER");
-	public String POSTGRES_PASSWORD=System.getenv("POSTGRES_PASSWORD");
-	public String RAILWAY_TCP_PROXY_DOMAIN=System.getenv("RAILWAY_TCP_PROXY_DOMAIN");
-	public String RAILWAY_TCP_PROXY_PORT=System.getenv("RAILWAY_TCP_PROXY_PORT");
-	public String PGDATABASE=System.getenv("PGDATABASE");
+
 	public static void main(String[] args) {
 		SpringApplication.run(LibroloomApplication.class, args);
 	}
